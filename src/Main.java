a1a25286500c3ca70bad29c6f3ffdaa195b15d4d
Kirill Grishin <Rigo656@mail.ru>
2022-02-03 15:40:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // входные данные
+
+        int price = 1575;
+        int bonus = price / 20; // каждые 20р. = 1 бонусной миле
+
+        //  логика
+
+        System.out.println("Бонусные мили с покупки: " + bonus);
+
+    }
+}
